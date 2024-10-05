4b25cf733f7bb1f1c6181bf059ef9b777c30e546
EricChan11 <115172673+EricChan11@users.noreply.github.com>
2024-10-05 14:50:19 +0800

    ch14

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -5,4 +5,7 @@ import com.bignerdranch.android.criminalintent.CrimeRepository
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
     val  crimeListLiveData = crimeRepository.getCrimes()
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
 }
